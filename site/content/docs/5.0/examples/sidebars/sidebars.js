11d7c7a0eeb6e3decccfe2e0edb18dda75ed2275
Mark Otto <markdotto@gmail.com>
2021-03-10 08:30:43 -0800

    Try JS fix

--- site/content/docs/5.0/examples/sidebars/sidebars.js
+++ site/content/docs/5.0/examples/sidebars/sidebars.js
@@ -1,4 +1,8 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+/* global bootstrap: false */
+(function () {
+  'use strict'
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
